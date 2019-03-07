7fd1099126dbc3aed5fb6227f0f323f1724a10b5
Loïc Mahieu <mahieuloic@gmail.com>
2019-03-07 17:36:29 +0100

    Demo for https://github.com/gatsbyjs/gatsby/issues/12272#issuecomment-470569911

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,21 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.createResolvers = ({ createResolvers }) => {
+  createResolvers({
+    CommentsJson: {
+      post: {
+        type: `PostsJson`,
+        resolve (source, args, context, info) {
+          const allNodes = context.nodeModel.getAllNodes()
+          return allNodes
+            .filter(node => node.internal.type === 'PostsJson')
+            .find(page => {
+              const parent = allNodes.find(n => n.id === page.parent)
+              return parent && parent.name === source.postId
+            })
+        }
+      }
+    }
+  })
+}
